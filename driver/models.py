47bc483ceec1c610897534f0d8bfbaafc1453518
acorrea-B <alejo95@unimayor.edu.co>
2022-11-07 11:50:53 -0500

    add driver model and request log to save response of API

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,32 @@
+from urllib import response
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Driver(models.Model):
+
+    id = models.BigAutoField(
+        primary_key=True
+    )
+    lat = models.DecimalField(
+        max_digits=22,
+        decimal_places=16,
+        blank=True,
+        null=True,
+        help_text = "location latitude Driver"
+    )
+    lng = models.DecimalField(
+        max_digits=22,
+        decimal_places=16,
+        blank=True,
+        null=True,
+        help_text = "location longitude Driver"
+    )
+    last_update = models.DateTimeField()
+
+class DriverRequestLog(models.Model):
+    creation_date = models.DateTimeField(
+        default=timezone.now()
+    )
+    response = models.JSONField()
+    
+    
